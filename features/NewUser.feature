ad23546d50b5319c48730dd1898fbeb76a37f8ec
rutger0714 <rutger0714@gmail.com>
2024-11-10 16:57:24 -0600

    Added a login_steps definition and A New User Scenario.

---
+++ features/NewUser.feature
@@ -0,0 +1,6 @@
+Feature: Move To SignUp Page to Sign Up a New User
+
+Scenario:  Move to Sign up page from Login Page
+  Given I am a New User
+  When I select "SignUp"
+  Then I should See the Sign Up page
---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,11 @@
+Given("I am a New User") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I select {string}") do |button|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should See the Sign Up page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
