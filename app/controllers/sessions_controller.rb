75ee2ce5b1a44a9e09e481665e13690745d6dd4d
Charley Dunham <charles-dunham@uiowa.edu>
2024-11-14 14:32:49 -0600

    updated session controller for userlogin and authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,5 +9,26 @@ class SessionsController < ApplicationController
   def login_menu
     render 'menus/login_menu'
   end
+
+  def create
+      # Real user authentication
+      user = User.find_by(email: params[:email])
+      if user&.authenticate(params[:password])
+        session[:user_id] = user.id
+        flash[:notice] = "Logged in successfully!"
+        redirect_to root_path
+      else
+        flash.now[:alert] = "Invalid email or password"
+        render 'menus/login_menu'
+      end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out successfully!"
+    redirect_to root_path
+  end
+
+
 end
 
