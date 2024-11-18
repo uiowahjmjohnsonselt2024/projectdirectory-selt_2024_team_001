dee09b5245ed463f3ac90c7d8f5f77e4a306b7ed
rutger0714 <rutger0714@gmail.com>
2024-11-18 16:48:28 -0600

    made a few changes

--- app/models/grid_tile.rb
+++ app/models/grid_tile.rb
@@ -1,7 +1,3 @@
 class GridTile < ApplicationRecord
-  belongs_to :server
-  has_many :users
 
-  validates :row, :column, presence: true, numericality: { only_integer: true }
-  validates :environment, :time_of_day, :weather, presence: true
 end
--- app/models/server.rb
+++ app/models/server.rb
@@ -1,24 +1,3 @@
 class Server < ApplicationRecord
-  has_many :grid_tiles, dependent: :destroy
-  has_many :users, dependent: :nullify
 
-  validates :name, presence: true, uniqueness: true
-
-  after_create :initialize_grid
-
-  private
-
-  def initialize_grid
-    (0..5).each do |row|
-      (0..5).each do |column|
-        self.grid_tiles.create!(
-          row: row,
-          column: column,
-          environment: ["forest", "desert", "ocean"].sample,
-          time_of_day: ["morning", "afternoon", "night"].sample,
-          weather: ["sunny", "rainy", "snowy"].sample
-        )
-      end
-    end
-  end
 end
