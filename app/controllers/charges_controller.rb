67c26abce2a039f050521c56c0004d6010a2df78
joBrady <brady.joe63@gmail.com>
2024-11-26 19:57:14 -0600

    Completed intial charges_controller.rb and Deleted .html.erb files to be replace by html.haml files

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,7 +1,38 @@
 class ChargesController < ApplicationController
-  def new
-  end
+  rescue_from Stripe::CardError, Stripe::InvalidRequestError, with: :catch_exception
+  before_action :set_params
 
   def create
+    charge = Stripe::Charge.create(amount: (@amount * 100), source: stripe_token.id, currency: 'usd')
+    flash[:notice] = charge[:paid] == true ? success_message(charge) : failure_message
+    redirect_back fallback_location: root_path
+  end
+
+  private
+
+  def set_params
+    @card_number = params[:card_number] || 0
+    @card_month = params[:month] || 0
+    @card_year = params[:year] || 0
+    @cvc = params[:cvc] || 0
+    @amount = params[:amount]&.to_i || 0
+  end
+
+  def stripe_token
+    Stripe::Token.create(card: { number: @card_number, exp_month: @card_month, exp_year: @card_year, cvc: @cvc })
+  end
+
+  def catch_exception(exception)
+    flash[:alert] = exception.message
+    redirect_back fallback_location: root_path
+  end
+
+  def success_message(charge)
+    "Your payment of amount: #{@amount} has been successfully processed.
+     You can take your receipt from here: #{charge[:receipt_url]}"
+  end
+
+  def failure_message
+    "We are sorry. We are unable to proceed with your request. Please try again later."
   end
 end
