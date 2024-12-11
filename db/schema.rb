b344ae87f76501f52af58de309915efd5fef5cb4
egladden35 <egladden@uiowa.edu>
2024-12-11 15:45:06 -0600

    i have created the migration for adding settings to the users database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_29_192559) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_11_213755) do
   create_table "achievements", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "name", null: false
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_29_192559) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "gold", default: 1000, null: false
+    t.json "settings"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
