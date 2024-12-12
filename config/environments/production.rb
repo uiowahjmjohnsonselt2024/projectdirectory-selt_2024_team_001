b0fc3475f54ac720dea52c6aaef27517c3edb2ba
joBrady <brady.joe63@gmail.com>
2024-12-11 18:30:52 -0600

    Saved a note about the Security Feature to add possibly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+  #HEY THERES THE MODIFCITATION FOR AN SSL APPLICATION
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
