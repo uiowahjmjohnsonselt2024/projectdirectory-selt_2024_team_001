6892f117ba0f9e1971d5bb4ee4bc44a116be3a4a
joBrady <brady.joe63@gmail.com>
2024-12-12 21:38:17 -0600

    Generated views for new_message_form and updated rooms_controller to adapt

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,9 +4,20 @@ class RoomsController < ApplicationController
     @room = Room.new
     @rooms = Room.public_rooms
 
+    @users = User.all_except(current_user)
+    render 'index'
+  end
+
+  def show
     @single_room = Room.find(params[:id])
+    @room = Room.new
+    @rooms = Room.public_rooms
+
+    @message = Message.new
+    @messages = @single_room.messages(created_at: :asc)
 
     @users = User.all_except(current_user)
+    render 'index'
   end
 
   def create
