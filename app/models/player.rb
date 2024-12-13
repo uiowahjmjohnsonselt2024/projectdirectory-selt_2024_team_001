d8f37dc5edc91aab518cb7586b52d188e806a793
Charley Dunham <charles-dunham@uiowa.edu>
2024-12-12 22:16:26 -0600

    updated to support player movement

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,11 @@
 class Player < ApplicationRecord
-  belongs_to :server
   belongs_to :user
+  belongs_to :server
 
-  validates :user_id, uniqueness: { scope: :server_id, message: "can only have one player per server" }
+  validates :row, :column, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  # Example method to update position
+  def update_position(new_row, new_column)
+    update!(row: new_row, column: new_column)
+  end
 end
