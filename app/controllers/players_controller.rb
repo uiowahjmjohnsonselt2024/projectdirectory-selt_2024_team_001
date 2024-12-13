f830e0b0766a62546a4604b360ba2d36df7a3871
Charley Dunham <charles-dunham@uiowa.edu>
2024-12-12 22:15:22 -0600

    updated players controller to handle movement

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,6 @@
 class PlayersController < ApplicationController
   before_action :set_server
+  before_action :set_player, only: [:update_position] # Add this for the new action
 
   def create
     @player = @server.players.new(player_params)
@@ -12,10 +13,28 @@ class PlayersController < ApplicationController
     end
   end
 
+  # New action to move a player to a specific tile
+  def update_position
+    player = Player.find(params[:id])
+    row = params[:row].to_i
+    column = params[:column].to_i
+
+    if player.update_position(row, column)
+      render json: { success: true }, status: :ok
+    else
+      render json: { error: "Failed to update position" }, status: :unprocessable_entity
+    end
+  end
+
+
   private
 
   def set_server
     @server = Server.find(params[:server_id])
+  end
+
+  def set_player
+    @player = @server.players.find(params[:id]) # Find player within the server
   end
 
   def player_params
