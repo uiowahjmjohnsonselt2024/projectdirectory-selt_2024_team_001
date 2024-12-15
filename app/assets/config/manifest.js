715b5b0759b949ffb33e915156489adb2b138fdf
Logan <loganclingner@gmail.com>
2024-12-14 23:16:46 -0600

    added storefront to the file

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,3 +4,4 @@
 //= link_tree ../../../vendor/javascript .js//= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 // = link_tree ../game_view
+// = link_tree ../storefront
