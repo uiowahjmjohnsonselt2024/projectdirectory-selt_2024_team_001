81dde66ed7e25ccaf23b7638be8f870d55762ba6
Charley Dunham <charles-dunham@uiowa.edu>
2024-12-15 04:29:44 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -94,8 +94,7 @@ Rails.application.routes.draw do
 
 
   get 'convert_to_usd', to: 'conversions#convert_to_usd'
-
-  patch 'toggle_theme', to: 'sessions#toggle_theme', as: :toggle_theme
+  
 
   post 'update_session_profile_picture', to: 'sessions#update_session_profile_picture'
 
